8fa24d0726fdd2df6169c05a9a43f44bcd30d2f2
Jihun Choi <98497929+Choijh03@users.noreply.github.com>
2023-03-01 22:41:41 -0800

    code updated
    code updated

---
+++ compass_app/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:math' as math;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _hasPermissions = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchPermissionStatus();
+  }
+
+  void _fetchPermissionStatus() {
+    Permission.locationWhenInUse.status.then((status) {
+      if (mounted) {
+        setState(() {
+          _hasPermissions = (status == PermissionStatus.granted);
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.amber[200],
+        body: Builder(
+          builder: (context) {
+            if (_hasPermissions) {
+              return _buildCompass();
+            } else {
+              return _buildPermissionSheet();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  //comppas widget
+  Widget _buildCompass() {
+    return StreamBuilder<CompassEvent>(
+      stream: FlutterCompass.events,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          // error msg
+          return Text('Error reading heading: ${snapshot.error}');
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        double? direction = snapshot.data!.heading;
+
+        //if direction is null, then device does not support this sensor
+        if (direction == null) {
+          return const Center(
+            child: Text('Device does not have sensor'),
+          );
+        }
+
+        return Center(
+          child: Container(
+            padding: const EdgeInsets.all(25),
+            child: Transform.rotate(
+              angle: direction * (math.pi / 180) * -1,
+              child: Image.asset(
+                'lib/images/compass.png',
+                color: Colors.black,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  //permission sheet widget
+  Widget _buildPermissionSheet() {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Request Permission'),
+        onPressed: () {
+          Permission.locationWhenInUse.request().then((value) {
+            _fetchPermissionStatus();
+          });
+        },
+      ),
+    );
+  }
+}
